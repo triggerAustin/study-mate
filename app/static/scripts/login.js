2ca3dccb4d3b68ede7f535da569882cfe47007df
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-09-08 00:55:20 +0100

    complete the UI pages

--- app/static/scripts/login.js
+++ app/static/scripts/login.js
@@ -2,18 +2,19 @@
 to the proper page student dashboard for the students and
 the teacher dashboard for the teachers
  */
-document.getElementById("login").addEventListener("submit", function(event){
-    event.preventDefault();
+document.getElementById("login").addEventListener("submit",
+    function (event) {
+        event.preventDefault();
 
-    /* collect the user data and send it to the server to validate the login */
-    /*austin please change the login to the proper endpoint */
-    let user_data = new FormData(document.getElementById("loginForm"));
+        /* collect the user data and send it to the server to validate the login */
+        /*austin please change the login to the proper endpoint */
+        let user_data = new FormData(document.getElementById("loginForm"));
 
-    fetch("/studyMate/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: user_data
-    })
-)
+        fetch("/studyMate/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: user_data
+        })
+    )
